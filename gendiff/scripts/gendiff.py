b85a16338633aefcfdaae04faf680b0bfe15da13
akocur <akocur@yandex.ru>
2021-08-17 11:49:41 +0300

    add --format option to cli

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,9 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    args = parser.parse_args()
+    parser.add_argument('-f', '--format',
+                        metavar='FORMAT', help='set format of output')
+    parser.parse_args()
 
 
 if __name__ == '__main__':
