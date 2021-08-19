ca72d3f2237876aa2eb940867799059eb2fbd1d1
akocur <akocur@yandex.ru>
2021-08-19 15:02:29 +0300

    modify gendiff/scripts/gendiff.py
    add type for first_file and second_file arguments

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,13 @@
 import argparse
+import pathlib
+
 from gendiff.gendiff import generate_diff
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=pathlib.Path)
+    parser.add_argument('second_file', type=pathlib.Path)
     parser.add_argument('-f', '--format',
                         metavar='FORMAT', help='set format of output')
     args = parser.parse_args()
