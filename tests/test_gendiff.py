660305ceb2c4be6b0157f4e5a447744addfb471a
akocur <akocur@yandex.ru>
2021-08-21 16:36:39 +0300

    modify tests/test_gendiff.py
    add test for yaml format

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -14,3 +14,7 @@ def test_generate_diff():
     file_path1 = 'tests/fixtures/file1.json'
     file_path2 = 'tests/fixtures/file2.json'
     assert generate_diff(file_path1, file_path2) == expected
+
+    file_path1 = 'tests/fixtures/file1.yaml'
+    file_path2 = 'tests/fixtures/file2.yaml'
+    assert generate_diff(file_path1, file_path2) == expected
