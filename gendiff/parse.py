c232f614e679efbdbd11492b9e6b49093aeae35f
akocur <akocur@yandex.ru>
2021-08-21 19:19:43 +0300

    refactoring parse function

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -15,20 +15,21 @@ def parse(data1, data2, file_type='json'):
             return json.dumps(value)
         return yaml.safe_dump(value)[:-5]
 
-    keys1 = data1.keys()
-    keys2 = data2.keys()
-    only_data1 = ((key, get_line('-', key, convert_value(data1.get(key))))
-                  for key in keys1 - keys2)
-    only_data2 = ((key, get_line('+', key, convert_value(data2.get(key))))
-                  for key in keys2 - keys1)
+    def difference(dict1, dict2, sign):
+        return ((key, get_line(sign, key, convert_value(dict1.get(key))))
+                for key in dict1.keys() - dict2.keys())
+
+    only_data1 = difference(data1, data2, '-')
+    only_data2 = difference(data2, data1, '+')
+    key_intersection = data1.keys() & data2.keys()
     equal_values = ((key, get_line(' ', key, convert_value(data2.get(key))))
                     for key in filter(lambda k: data1.get(k) == data2.get(k),
-                                      keys1 & keys2))
+                                      key_intersection))
     different_values = ((key, '\n'.join(
         [get_line('-', key, convert_value(data1.get(key))),
          get_line('+', key, convert_value(data2.get(key))),
          ])) for key in filter(lambda k: data1.get(k) != data2.get(k),
-                               keys1 & keys2))
+                               key_intersection))
     result = ['{']
     result.extend(map(itemgetter(1),
                       sorted(chain(only_data1,
