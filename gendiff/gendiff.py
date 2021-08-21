8d116a24e0c475784688b5ce4aa3e890bbce7a1a
akocur <akocur@yandex.ru>
2021-08-21 18:53:35 +0300

    modify gendiff/gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,40 +1,23 @@
 import json
-from operator import itemgetter
-from itertools import chain
+import yaml
+from gendiff.parse import parse
 
 
 def generate_diff(file_path1, file_path2):
-    def get_line(sign, key, value):
-        return '  {} {}: {}'.format(sign, key, value)
+    data1 = load(file_path1)
+    data2 = load(file_path2)
+    return parse(data1, data2, file_type(file_path1))
 
-    def value_to_json(value):
-        if isinstance(value, str):
-            return value
-        return json.dumps(value)
 
-    json1 = json.load(open(file_path1))
-    json2 = json.load(open(file_path2))
-    keys1 = json1.keys()
-    keys2 = json2.keys()
-    only_json1 = ((key, get_line('-', key, value_to_json(json1.get(key))))
-                  for key in keys1 - keys2)
-    only_json2 = ((key, get_line('+', key, value_to_json(json2.get(key))))
-                  for key in keys2 - keys1)
-    equal_values = ((key, get_line(' ', key, value_to_json(json2.get(key))))
-                    for key in filter(lambda k: json1.get(k) == json2.get(k),
-                                      keys1 & keys2))
-    different_values = ((key, '\n'.join(
-        [get_line('-', key, value_to_json(json1.get(key))),
-         get_line('+', key, value_to_json(json2.get(key))),
-         ]))
-        for key in filter(lambda k: json1.get(k) != json2.get(k),
-                          keys1 & keys2))
-    result = ['{']
-    result.extend(map(itemgetter(1),
-                      sorted(chain(only_json1,
-                                   only_json2,
-                                   equal_values,
-                                   different_values))))
-    result.append('}')
-    print(result)
-    return '\n'.join(result)
+def load(file_path):
+    if file_type(file_path) == 'yaml':
+        return yaml.safe_load(open(file_path))
+    return json.load(open(file_path))
+
+
+def file_type(file_path):
+    if file_path.endswith(('.yaml', '.yml')):
+        return 'yaml'
+    if file_path.endswith('.json'):
+        return 'json'
+    return None
