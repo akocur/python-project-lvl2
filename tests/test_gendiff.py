3a9b0d973da83d7109afa41a173e34397fd1ce56
akocur <akocur@yandex.ru>
2021-08-30 14:46:05 +0300

    add test for format=plain

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -10,3 +10,8 @@ def test_generate_diff():
     file_path1 = 'tests/fixtures/file1.yaml'
     file_path2 = 'tests/fixtures/file2.yaml'
     assert generate_diff(file_path1, file_path2) == expected
+
+    file_path1 = 'tests/fixtures/file1.yaml'
+    file_path2 = 'tests/fixtures/file2.yaml'
+    expected = open('tests/fixtures/diff_plain')
+    assert generate_diff(file_path1, file_path2, 'plain') == expected
