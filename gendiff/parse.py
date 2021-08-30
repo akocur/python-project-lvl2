8a1a0a9af11fd76fd0315ba3083db3c76d20b36d
akocur <akocur@yandex.ru>
2021-08-30 12:06:42 +0300

    modify gendiff/parse.py

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -1,38 +1,13 @@
-from itertools import chain
-from operator import itemgetter
+from gendiff.diff import make_diff
+from gendiff.diff import get_keys_values
 
 
-def parse(data1, data2, file_type='json'):
-    def get_line(sign, key, value):
-        return '  {} {}: {}'.format(sign, key, value)
-
-    def convert_value(value):
-        if value is None:
-            return 'null'
-        return str(value).lower()
-
-    def difference(dict1, dict2, sign):
-        return ((key, get_line(sign, key, convert_value(dict1.get(key))))
-                for key in dict1.keys() - dict2.keys())
-
-    only_from_data1 = difference(data1, data2, '-')
-    only_from_data2 = difference(data2, data1, '+')
-    common_keys = data1.keys() & data2.keys()
-    equal_values = ((key, get_line(' ', key, convert_value(data2.get(key))))
-                    for key in filter(lambda k: data1.get(k) == data2.get(k),
-                                      common_keys))
-    different_values = ((key, '\n'.join(
-        [get_line('-', key, convert_value(data1.get(key))),
-         get_line('+', key, convert_value(data2.get(key))),
-         ])) for key in filter(lambda k: data1.get(k) != data2.get(k),
-                               common_keys))
-    result = ['{']
-    result.extend(map(itemgetter(1),
-                      sorted(chain(only_from_data1,
-                                   only_from_data2,
-                                   equal_values,
-                                   different_values))))
-    result.append('}')
-
-    print(result)
-    return '\n'.join(result)
+def parse(data1, data2):
+    """
+    Return list diffs.
+    :param data1: dict
+    :param data2: dict
+    :return: list of diffs
+    """
+    diffs = list(map(lambda x: make_diff(*x), get_keys_values(data1, data2)))
+    return diffs
