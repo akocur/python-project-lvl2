8bbf0c992e2859c53cefbf2c0c88ecb33e873283
akocur <akocur@yandex.ru>
2021-08-30 12:02:39 +0300

    modify test_generate_diff()

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,15 +2,7 @@ from gendiff.gendiff import generate_diff
 
 
 def test_generate_diff():
-    expected = """{
-  - follow: false
-    host: hexlet.io
-  + none: null
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
+    expected = open('tests/fixtures/diff').read()
     file_path1 = 'tests/fixtures/file1.json'
     file_path2 = 'tests/fixtures/file2.json'
     assert generate_diff(file_path1, file_path2) == expected
