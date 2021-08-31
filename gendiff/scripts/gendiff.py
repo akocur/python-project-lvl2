9ada0cd115bfefb52308a041b52b6b840b6bd952
akocur <akocur@yandex.ru>
2021-08-31 13:43:50 +0300

    modify --format argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,9 +8,12 @@ def main():
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format',
-                        metavar='FORMAT', help='set format of output')
+                        choices=['stylish', 'plain'],
+                        default='stylish',
+                        metavar='FORMAT',
+                        help='set format of output')
     args = parser.parse_args()
-    diff = generate_diff(args.first_file, args.second_file)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
 
 
