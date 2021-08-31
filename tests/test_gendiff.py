a705163f3b5351cff9abfc8af7147827418aa5a5
akocur <akocur@yandex.ru>
2021-08-31 12:54:04 +0300

    modify tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,7 +11,7 @@ def test_generate_diff():
     file_path2 = 'tests/fixtures/file2.yaml'
     assert generate_diff(file_path1, file_path2) == expected
 
-    # file_path1 = 'tests/fixtures/file1.yaml'
-    # file_path2 = 'tests/fixtures/file2.yaml'
-    # expected = open('tests/fixtures/diff_plain')
-    # assert generate_diff(file_path1, file_path2, 'plain') == expected
+    file_path1 = 'tests/fixtures/file1.yaml'
+    file_path2 = 'tests/fixtures/file2.yaml'
+    expected = open('tests/fixtures/diff_plain').read()
+    assert generate_diff(file_path1, file_path2, 'plain') == expected
