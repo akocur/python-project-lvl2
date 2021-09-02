0c4cbf868c7c2e72f562a784b038856b0d07a927
akocur <akocur@yandex.ru>
2021-09-02 16:40:07 +0300

    refactor gendiff/parse.py
    add docstring for function.

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -4,10 +4,13 @@ from gendiff.diff import get_keys_values
 
 def parse(data1, data2):
     """
-    Return list diffs.
+    Return list of diff.
+
+    diff is abstraction. See make_diff function.
+
     :param data1: dict
     :param data2: dict
-    :return: list of diffs
+    :return: list of diff
     """
     diffs = list(map(lambda x: make_diff(*x), get_keys_values(data1, data2)))
     return diffs
