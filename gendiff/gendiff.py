c7d0340eb32d86bf7225739a73118110d95cc121
akocur <akocur@yandex.ru>
2021-09-02 16:42:18 +0300

    refactor gendiff/gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -7,19 +7,31 @@ from gendiff.parse import parse
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
-    data1 = load(file_path1)
-    data2 = load(file_path2)
+    """
+    Generate diff between file_path1 and file_path2 files.
+
+    Return result in format_name format.
+    The available formats are described in the get_formatter function from
+    gendiff.formats.
+
+    :param file_path1: str
+    :param file_path2: str
+    :param format_name: str
+    :return: str
+    """
+    data1 = _load(file_path1)
+    data2 = _load(file_path2)
     format_ = get_formatter(format_name)
     return format_(parse(data1, data2))
 
 
-def load(file_path):
-    if file_type(file_path) == 'yaml':
+def _load(file_path):
+    if _file_type(file_path) == 'yaml':
         return yaml.safe_load(open(file_path))
     return json.load(open(file_path))
 
 
-def file_type(file_path):
+def _file_type(file_path):
     if file_path.endswith(('.yaml', '.yml')):
         return 'yaml'
     if file_path.endswith('.json'):
