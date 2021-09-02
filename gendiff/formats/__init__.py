1c67c06aff8ca7f1d02b4891f734aa78bdc8b9d1
akocur <akocur@yandex.ru>
2021-09-02 17:00:11 +0300

    refactor gendiff/formats/__init__.py
    add new functions. add docstring for functions

--- gendiff/formats/__init__.py
+++ gendiff/formats/__init__.py
@@ -2,10 +2,31 @@ import gendiff.formats.plain
 import gendiff.formats.stylish
 import gendiff.formats.json
 
+FORMAT_PLAIN = 'plain'
+FORMAT_JSON = 'json'
+FORMAT_STYLISH = 'stylish'
+
 
 def get_formatter(format_name):
-    if format_name == 'plain':
+    """
+    Return formatter by format_name.
+
+    :param format_name: str
+        one of the available formatters.
+    :return: formatting function
+    """
+    if format_name == FORMAT_PLAIN:
         return gendiff.formats.plain.plain
-    if format_name == 'json':
+    if format_name == FORMAT_JSON:
         return gendiff.formats.json.formatted_to_json
+    return get_default_formatter()
+
+
+def get_default_formatter():
+    """Return default formatter."""
     return gendiff.formats.stylish.stylish
+
+
+def get_available_formatters():
+    """Return available formatters."""
+    return [FORMAT_STYLISH, FORMAT_PLAIN, FORMAT_JSON]
