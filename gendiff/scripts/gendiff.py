f51cf4722b53a5c8aeecb0e483cce6cbf3311390
akocur <akocur@yandex.ru>
2021-09-02 19:26:18 +0300

    refactor gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,26 @@
 import argparse
 
+from gendiff.formats import get_available_formatters
+from gendiff.formats import get_default_format_name
 from gendiff.gendiff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate diff')
+    """Entry point."""
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format',
-                        choices=['stylish', 'plain', 'json'],
-                        default='stylish',
-                        metavar='FORMAT',
-                        help='set format of output')
+    parser.add_argument(
+        '-f', '--format',
+        choices=get_available_formatters(),
+        default=get_default_format_name(),
+        metavar='FORMAT',
+        help='output format (default: "{}"). Available: {}'.format(
+            get_default_format_name(), get_available_formatters()
+        )
+    )
     args = parser.parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
