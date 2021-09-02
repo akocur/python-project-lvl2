c69ce68371f87e8aee128263196eea8706a74dad
akocur <akocur@yandex.ru>
2021-09-02 18:21:13 +0300

    refactor gendiff/formats/json.py

--- gendiff/formats/json.py
+++ gendiff/formats/json.py
@@ -5,6 +5,32 @@ import gendiff.diff as diff
 
 
 def formatted_to_json(diffs):
+    """
+    Format the diff list to json format.
+
+    Output ordered by key from diff.
+
+    Example:
+    [
+        {
+            "key": "group3",
+            "status": "added",
+            "old_value": null,
+            "new_value": {
+              "deep": {
+                "id": {
+                  "number": 45
+                }
+              },
+              "fee": 100500
+            },
+            "children": null
+        }
+    ]
+
+    :param diffs: list of diff. See gendiff.diff package.
+    :return: str
+    """
 
     def sort_children(current_diff):
         children = diff.get_children(current_diff)
